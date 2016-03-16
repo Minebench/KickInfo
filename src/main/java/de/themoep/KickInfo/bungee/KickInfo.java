c016313275033f43dae996f04ce0ae37d4ba55b7
Max Lee <Phoenix616@users.noreply.github.com>
2016-03-16 16:47:42 +0100

    Remove version checks as they aren't reliable, just don't specially handle players which got kicked because of outdated clients or servers.

--- src/main/java/de/themoep/KickInfo/bungee/KickInfo.java
+++ src/main/java/de/themoep/KickInfo/bungee/KickInfo.java
@@ -3,14 +3,12 @@ package de.themoep.KickInfo.bungee;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.Title;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.event.EventHandler;
 
-import java.util.ArrayList;
 import java.util.logging.Level;
 
 /**
@@ -24,24 +22,21 @@ public class KickInfo extends Plugin implements Listener {
 
     @EventHandler
     public void onServerKick(ServerKickEvent event) {
+        if(event.getKickReason().toLowerCase().startsWith("outdated")) {
+            return;
+        }
         String msg;
-        boolean onDefaultServer = event.getKickedFrom().getName().equals(event.getPlayer().getPendingConnection().getListener().getDefaultServer());
-        boolean onFallbackServer = event.getKickedFrom().getName().equals(event.getPlayer().getPendingConnection().getListener().getFallbackServer());
-        int serverVersion = event.getKickedFrom().getPlayers().size() > 0 ? new ArrayList<ProxiedPlayer>(event.getKickedFrom().getPlayers()).get(0).getPendingConnection().getVersion() : 0;
-        int cancelServerVersion = event.getCancelServer() != null && event.getCancelServer().getPlayers().size() > 0 ? new ArrayList<ProxiedPlayer>(event.getCancelServer().getPlayers()).get(0).getPendingConnection().getVersion() : 0;
-        if(event.getCancelServer() == null || (onDefaultServer || onFallbackServer) && cancelServerVersion == event.getPlayer().getPendingConnection().getVersion()) {
+        if(event.getPlayer().getPendingConnection().getListener().getServerPriority().contains(event.getKickedFrom().getName())) {
             event.getPlayer().disconnect(event.getKickReasonComponent());
             msg = " from proxy";
-        } else if(serverVersion == event.getPlayer().getPendingConnection().getVersion()){
+        } else {
             event.getPlayer().sendMessage(ChatColor.GOLD + "Du wurdest von " + event.getKickedFrom().getName() + " gekickt! Grund: " + event.getKickReason());
             Title title = getProxy().createTitle();
             title.title(TextComponent.fromLegacyText(ChatColor.GOLD + "Auf Lobby gekickt!"));
             title.subTitle(new ComponentBuilder("Grund: ").color(ChatColor.RED).append(event.getKickReason()).color(ChatColor.YELLOW).create());
             title.fadeIn(20).stay(100).fadeOut(20);
             event.getPlayer().sendTitle(title);
-            msg = " to lobby";
-        } else {
-            msg = "";
+            msg = " to fallback server";
         }
         getLogger().log(Level.INFO, "Kicked " + event.getPlayer().getName() + " from " + event.getKickedFrom().getName() + msg + ": " + event.getKickReason());
     }
