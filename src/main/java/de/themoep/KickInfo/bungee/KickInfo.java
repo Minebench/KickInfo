0fa3544fbbe9d2ffc4d1f05e68297c9daa5cb53d
Max Lee <Phoenix616@users.noreply.github.com>
2016-03-16 17:15:51 +0100

    This should ensure better that the player ends up at the right server or kicked

--- src/main/java/de/themoep/KickInfo/bungee/KickInfo.java
+++ src/main/java/de/themoep/KickInfo/bungee/KickInfo.java
@@ -3,14 +3,13 @@ package de.themoep.KickInfo.bungee;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.Title;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.config.ListenerInfo;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.event.EventHandler;
 
+import java.util.List;
 import java.util.logging.Level;
 
 /**
@@ -24,33 +23,20 @@ public class KickInfo extends Plugin implements Listener {
 
     @EventHandler
     public void onServerKick(ServerKickEvent event) {
-        getLogger().log(Level.INFO, "Kicked " + event.getPlayer().getName() + " from " + event.getKickedFrom().getName() + ": " + event.getKickReason());
-        if(event.getKickReason().toLowerCase().startsWith("outdated")) {
-            ListenerInfo l = event.getPlayer().getPendingConnection().getListener();
-            if(l.getServerPriority().size() <= 1) {
-                return;
-            }
-            int i = l.getServerPriority().indexOf(event.getKickedFrom().getName());
-            if(i != -1 && l.getServerPriority().size() > i + 1) {
-                ServerInfo s = getProxy().getServerInfo(l.getServerPriority().get(i + 1));
-                event.setCancelServer(s);
-                event.setCancelled(true);
-            }
-            return;
-        }
-        String msg;
-        if(event.getPlayer().getPendingConnection().getListener().getServerPriority().contains(event.getKickedFrom().getName())) {
-            event.getPlayer().disconnect(event.getKickReasonComponent());
-            msg = "From proxy";
-        } else {
+        getLogger().log(Level.INFO, event.getPlayer().getName() + " was kicked from " + event.getKickedFrom().getName() + ": " + event.getKickReason());
+        List<String> priority = event.getPlayer().getPendingConnection().getListener().getServerPriority();
+        if(!priority.contains(event.getKickedFrom().getName())) {
             event.getPlayer().sendMessage(ChatColor.GOLD + "Du wurdest von " + event.getKickedFrom().getName() + " gekickt! Grund: " + event.getKickReason());
             Title title = getProxy().createTitle();
             title.title(TextComponent.fromLegacyText(ChatColor.GOLD + "Auf Lobby gekickt!"));
             title.subTitle(new ComponentBuilder("Grund: ").color(ChatColor.RED).append(event.getKickReason()).color(ChatColor.YELLOW).create());
             title.fadeIn(20).stay(100).fadeOut(20);
             event.getPlayer().sendTitle(title);
-            msg = "To fallback server";
+            getLogger().log(Level.INFO, "To fallback server");
+        } else if(priority.indexOf(event.getKickedFrom().getName()) == priority.size() - 1) {
+            event.getPlayer().disconnect(event.getKickReasonComponent());
+        } else {
+            getLogger().log(Level.INFO, "To another fallback server");
         }
-        getLogger().log(Level.INFO, msg);
     }
 }
